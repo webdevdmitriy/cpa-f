49d386f273518e694822e2ce96d146e39472bf0d
Artem <tema25062010@gmail.com>
2026-05-23 01:52:09 +0300

    feat: create Button component

---
+++ src/components/VolumetricButton/Button.tsx
@@ -0,0 +1,11 @@
+import './button.scss';
+
+const VolumetricButton = () => {
+    return (
+        <div className="volumetric">
+            <button className="volumetric__btn">partner up</button>
+        </div>
+    )
+}
+
+export default VolumetricButton;
