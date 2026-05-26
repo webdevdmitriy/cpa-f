cb40972239ee0f774566c03e2685580768c174e1
Artem <tema25062010@gmail.com>
2026-05-26 21:39:05 +0300

    refactor: use prettier, code refactor

--- src/components/MultiTasks/MultiTasks.tsx
+++ src/components/MultiTasks/MultiTasks.tsx
@@ -1,57 +1,68 @@
 import styles from './multiTasks.module.scss';
 
 import Image from 'next/image';
-import MultiTasksImage from '@/assets/MultiTasksImage.png'
+import snake_5 from '@/assets/images/snake_5.png';
+
+import MultiTasksCard from './MultiTasksCard/MultiTasksCard';
+
+const cardsInfo = {
+  middle: [
+    {
+      id: 1,
+      label: 'Flexible infrastructure',
+      description: 'Custom tools, fast integrations and scalable architecture'
+    },
+    {
+      id: 2,
+      label: 'High-performing creatives',
+      description: 'Scroll-stopping ads tailored to your vertical'
+    }
+  ],
+  little: [
+    {
+      id: 3,
+      label: 'Compelling copywriting',
+      description: 'Messaging that hooks, sells, and drives funnel growth'
+    },
+    {
+      id: 4,
+      label: 'Adaptive\n media buying',
+      description: 'No wasted budgets — we test, tweak, and scale'
+    },
+    {
+      id: 5,
+      label: 'Full-cycle support',
+      description: 'From setup to scaling — we support you every step of the way'
+    }
+  ]
+};
 
 export default function MultiTasks() {
-    return (
-        <div className={styles.tasks}>
-            <div className='page_container'>
-                <h2 className={styles.header}>multi-tasks</h2>
-                <div className={styles.cards}>
-                    <div className={`${styles.info} ${styles.cardsWrapper}`}>
-                        <p>
-                            We run an <span>in-house team</span> of media <br/> buyers, designers, creatives, developers, 
-                            <br/> and copywriters — no middlemen, <br/> no outsourcing
-                        </p>
-                        <Image src={MultiTasksImage} alt='snake'></Image>
-                    </div>
-                    <div className={styles.cardsWrapper}>
-                        <div className={styles.card}>
-                            <h3 className={styles.cardHeader}>Flexible <br/> infrastructure</h3>
-                            <p className={styles.cardDescription}>
-                                Custom tools, fast integrations <br/> and scalable architecture
-                            </p>
-                        </div>
-                        <div className={styles.card}>
-                            <h3 className={styles.cardHeader}>High-performing <br/> creatives</h3>
-                            <p className={styles.cardDescription}>
-                                Scroll-stopping ads tailored to <br/> your vertical
-                            </p>
-                        </div>
-                    </div>
-                    <div className={styles.cardsWrapper}>
-                        <div className={styles.card}>
-                            <h3 className={styles.cardHeader}>Compelling <br/> copywriting</h3>
-                            <p className={styles.cardDescription}>
-                                Messaging that hooks, sells, and <br/> drives funnel growth
-                            </p>
-                        </div>
-                        <div className={styles.card}>
-                            <h3 className={styles.cardHeader}>Adaptive <br/> media buying</h3>
-                            <p className={styles.cardDescription}>
-                                No wasted budgets — we test, <br/> tweak, and scale
-                            </p>
-                        </div>
-                        <div className={styles.card}>
-                            <h3 className={styles.cardHeader}>Full-cycle <br/> support</h3>
-                            <p className={styles.cardDescription}>
-                                From setup to scaling — <br/> we support you every step of <br/> the way
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.tasks}>
+      <div className={styles.container}>
+        <h2 className={styles.header}>multi-tasks</h2>
+        <div className={styles.cards}>
+          <div className={`${styles.info} ${styles.cardsWrapper}`}>
+            <p>
+              We run an <span>in-house team</span> of media <br /> buyers, designers, creatives,
+              developers,
+              <br /> and copywriters — no middlemen, <br /> no outsourcing
+            </p>
+            <Image src={snake_5} alt="snake"></Image>
+          </div>
+          <div className={styles.cardsWrapper}>
+            {cardsInfo.middle.map(item => (
+              <MultiTasksCard key={item.id} label={item.label} description={item.description} />
+            ))}
+          </div>
+          <div className={styles.cardsWrapper}>
+            {cardsInfo.little.map(item => (
+              <MultiTasksCard key={item.id} label={item.label} description={item.description} />
+            ))}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
