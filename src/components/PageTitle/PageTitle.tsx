26d40e8bcabe66254f1efa7acc7573873233bf90
Yuri Yurin <yurin.yuri952@gmail.com>
2026-05-27 22:19:35 +0300

    feature-title-component

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,13 +1,14 @@
-import { ComponentProps } from 'react';
-import styles from './PageTitle.module.scss';
-import cn from 'classnames';
+import { ComponentProps } from 'react'
+import styles from './PageTitle.module.scss'
+import cn from 'classnames'
 
-type TPageTitleProps = { text: string; isUpperCase?: boolean } & ComponentProps<'div'>;
+type TPageTitleProps = { text: string; isUpperCase?: boolean } & ComponentProps<'div'>
 
 export const PageTitle = ({ text, isUpperCase = true, ...props }: TPageTitleProps) => {
+  const titleText = isUpperCase ? text.toUpperCase() : text
   return (
     <div className={cn(styles.title, props.className)} {...props}>
-      <span>{isUpperCase ? text.toUpperCase() : text}</span>
+      <span>{titleText}</span>
     </div>
-  );
-};
+  )
+}
