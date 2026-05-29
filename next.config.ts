c130011c708cfd294e3f7b340f972968316af351
Dmitriy <plotnikovdi95@gmail.com>
2026-05-29 15:54:30 +0300

    implimented inter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   sassOptions: {
     loadPaths: ['src'],
@@ -11,4 +13,4 @@ const nextConfig: NextConfig = {
   }
 }
 
-export default nextConfig
+export default withNextIntl(nextConfig)
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,14 @@
+import { useTranslations } from 'next-intl'
 import { PageTitle } from '@/components/PageTitle/PageTitle'
 import VolumetricButton from '@/components/VolumetricButton/Button'
 
 export default function Home() {
+  const t = useTranslations('HomePage')
+
   return (
     <main>
-      <PageTitle text="Hi. Im title" />
-      <VolumetricButton label="partner up" />
+      <PageTitle text={t('title')} />
+      <VolumetricButton label={t('button')} />
     </main>
   )
 }
