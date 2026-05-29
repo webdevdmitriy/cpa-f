b358cf556c02db1177eba6f3ad78136fdd052627
Artem <tema25062010@gmail.com>
2026-05-29 15:04:02 +0300

    refactor: code refactor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,24 @@
-import styles from './header.module.scss'
+import styles from './Header.module.scss'
 
 import { SnakeHeadIcon } from '@/assets/icons/SnakeHeadIcon'
+import cn from 'classnames'
 
 import Link from 'next/link'
 
 export default function Header() {
   return (
-    <nav className={styles.header}>
-      <SnakeHeadIcon className={styles.icon} />
+    <header className={styles.header}>
+      <SnakeHeadIcon />
       <div className={styles.links}>
         <Link href="#">Team</Link>
         <Link href="#">Benefits</Link>
         <Link href="#">Join Us</Link>
         <div className={styles.languages}>
-          <span className={`${styles.language} ${styles.active}`}>Eng</span>/
+          <span className={cn(styles.language, styles.active)}>Eng</span>
+          <span className={styles.slash}>/</span>
           <span className={styles.language}>Рус</span>
         </div>
       </div>
-    </nav>
+    </header>
   )
 }
